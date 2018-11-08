3d6c80f440d33057a8add79783be74ad9f601f68
dell <szekesililla93@gmail.com>
2018-11-08 10:34:49 +0100

    Main layout without score bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'stf';
 
-  
+  cards = {
+      0: 'src/app/cards/angular.png',
+      1: 'src/app/cards/d3.png',
+      2: 'src/app/cards/jenkins.png',
+      3: 'src/app/cards/postcss.png',
+      4: 'src/app/cards/react.png',
+      5: 'src/app/cards/redux.png',
+      6: 'src/app/cards/sass.png',
+      7: 'src/app/cards/supercharge.png',
+      8: 'src/app/cards/ts.png',
+      9: 'src/app/cards/webpack.png'
+  };
+
+  nSizedArray(n: number) {
+      return Array.from(new Array(n * 2), (val, index) => index % n);
+  }
+
+  flipCard(id: number) {
+      console.log(id);
+  }
 }
