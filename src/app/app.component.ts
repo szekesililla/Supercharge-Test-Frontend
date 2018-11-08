4c2336c98b163121e3889523144d21a5ed56b9f9
dell <szekesililla93@gmail.com>
2018-11-08 11:54:18 +0100

    You can flip cards and find pairs. Yeah, in a very hardcoded way...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,70 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'stf';
 
+  //  card id, card img path, is downside, is pair
   cards = {
-      0: 'src/app/cards/angular.png',
-      1: 'src/app/cards/d3.png',
-      2: 'src/app/cards/jenkins.png',
-      3: 'src/app/cards/postcss.png',
-      4: 'src/app/cards/react.png',
-      5: 'src/app/cards/redux.png',
-      6: 'src/app/cards/sass.png',
-      7: 'src/app/cards/supercharge.png',
-      8: 'src/app/cards/ts.png',
-      9: 'src/app/cards/webpack.png'
+      0: [0, 'src/app/cards/angular.png', true, true],
+      1: [1, 'src/app/cards/d3.png', true, true],
+      2: [2, 'src/app/cards/jenkins.png', true, true],
+      3: [3, 'src/app/cards/postcss.png', true, true],
+      4: [4, 'src/app/cards/react.png', true, true],
+      5: [5, 'src/app/cards/redux.png', true, true],
+      6: [6, 'src/app/cards/sass.png', true, true],
+      7: [7, 'src/app/cards/supercharge.png', true, true],
+      8: [8, 'src/app/cards/ts.png', true, true],
+      9: [9, 'src/app/cards/webpack.png', true, true],
+      10: [0, 'src/app/cards/angular.png', true, true],
+      11: [1, 'src/app/cards/d3.png', true, true],
+      12: [2, 'src/app/cards/jenkins.png', true, true],
+      13: [3, 'src/app/cards/postcss.png', true, true],
+      14: [4, 'src/app/cards/react.png', true, true],
+      15: [5, 'src/app/cards/redux.png', true, true],
+      16: [6, 'src/app/cards/sass.png', true, true],
+      17: [7, 'src/app/cards/supercharge.png', true, true],
+      18: [8, 'src/app/cards/ts.png', true, true],
+      19: [9, 'src/app/cards/webpack.png', true, true]
   };
 
+  numOfCards = Object.keys(this.cards).length;
+  countGuess = 0;
+  guess1 = -1;
+  guess2 = -1;
+  countPairs = 0;
+  prevWasPair = true;
+
   nSizedArray(n: number) {
-      return Array.from(new Array(n * 2), (val, index) => index % n);
+      return Array.from(new Array(n), (val, index) => index);
   }
 
   flipCard(id: number) {
-      console.log(id);
+      this.countGuess++;
+      this.cards[id][2] = false;
+      if (this.countGuess % 2 === 1) {
+          if (!this.prevWasPair) {
+              this.cards[this.guess1][2] = true;
+              this.cards[this.guess2][2] = true;
+          }
+          this.guess1 = id;
+      } else {
+          this.guess2 = id;
+          this.checkIfPair();
+      }
   }
+
+  checkIfPair() {
+      if (this.cards[this.guess1][0] === this.cards[this.guess2][0]) {
+          this.countPairs++;
+          this.prevWasPair = true;
+          if (this.isWin()) {
+              console.log('win');
+          }
+      } else {
+          this.prevWasPair = false;
+      }
+  }
+
+  isWin() {
+      return this.numOfCards / 2 === this.countPairs;
+  }
+
 }
